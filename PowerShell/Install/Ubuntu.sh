ea7b16329ffd6c86cf8a59acd407d0516c520b3b
Gabriel Vanca <gabriel@vanca.co.uk>
2022-11-06 11:18:23 +0000

    Ubuntu Powershell install

---
+++ PowerShell/Install/Ubuntu.sh
@@ -0,0 +1,17 @@
+# Source: https://learn.microsoft.com/en-gb/powershell/scripting/install/install-ubuntu?view=powershell-7.2
+
+
+# Update the list of packages
+sudo apt-get update
+# Install pre-requisite packages.
+sudo apt-get install -y wget apt-transport-https software-properties-common
+# Download the Microsoft repository GPG keys
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
+# Install PowerShell
+sudo apt-get install -y powershell
+# Start PowerShell
+pwsh
