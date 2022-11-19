1a0ee819d9868c957e0c63b1ae9e4adf1de0799f
Gabriel Vanca <gabriel@vanca.co.uk>
2022-11-19 14:44:08 +0000

    test

--- PowerShell/Install/Configure/Install_Oh-my-Posh.ps1
+++ PowerShell/Install/Configure/Install_Oh-my-Posh.ps1
@@ -2,25 +2,25 @@
 
 Write-Host "Installing Oh-My-Posh"
 
-if($IsWindows) {
-    $osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
-    if($osInfo.ProductType -eq 1) {
-        Write-Host "Windows workstation (Windows 10/11) deployment detected. Installing via winget"
-        winget install JanDeDobbeleer.OhMyPosh -s winget
-    } else {
-        Write-Host "Windows Server deployment detected. Installing via Chocolatey"
-        choco install oh-my-posh -y
-    }
-} else {
-    if($IsLinux) {
-        Write-Host "Linux deployment detected. Installing via Github"
-        wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-        sudo chmod +x /usr/local/bin/oh-my-posh
-        #Source: https://ohmyposh.dev/docs/installation/linux
-    } else {
-        if($IsMacOS) {
-            Write-Host "MacOS deployment detected. Installing via Homebrew"
-            brew install jandedobbeleer/oh-my-posh/oh-my-posh
-        }
-    }
-}
+# if($IsWindows) {
+#     $osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
+#     if($osInfo.ProductType -eq 1) {
+#         Write-Host "Windows workstation (Windows 10/11) deployment detected. Installing via winget"
+#         winget install JanDeDobbeleer.OhMyPosh -s winget
+#     } else {
+#         Write-Host "Windows Server deployment detected. Installing via Chocolatey"
+#         choco install oh-my-posh -y
+#     }
+# } else {
+#     if($IsLinux) {
+#         Write-Host "Linux deployment detected. Installing via Github"
+#         wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+#         sudo chmod +x /usr/local/bin/oh-my-posh
+#         #Source: https://ohmyposh.dev/docs/installation/linux
+#     } else {
+#         if($IsMacOS) {
+#             Write-Host "MacOS deployment detected. Installing via Homebrew"
+#             brew install jandedobbeleer/oh-my-posh/oh-my-posh
+#         }
+#     }
+# }
