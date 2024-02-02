b53dadff3a97f2328982be9edac5b33d600e5daa
Gabriel Vanca <gabriel@vanca.co.uk>
2024-02-02 20:23:39 +0000

    Theme

---
+++ PowerShell/Theme/Export_Theme_Demo.ps1
@@ -0,0 +1,6 @@
+
+#   PowerShell Theme with Galaxy Terminal Background:
+oh-my-posh config export image --cursor-padding 50 -o "D:\Theme\Arch_Theme.png" --author "Gabriel Vanca" --rprompt-offset 4 --background-color 1D2837
+  
+#   PowerShell Theme with standard black Background:
+oh-my-posh config export image --cursor-padding 50 -o "D:\Theme\Arch_Theme_(black_background).png" --author "Gabriel Vanca" --rprompt-offset 4
