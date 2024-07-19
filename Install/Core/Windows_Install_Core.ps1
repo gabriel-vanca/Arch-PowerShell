463601e81a0c39f5882a35496e97c4676f13d3f2
Gabriel Vanca <gabriel@vanca.co.uk>
2024-07-19 04:08:34 +0100

    format

--- Install/Core/Windows_Install_Core.ps1
+++ Install/Core/Windows_Install_Core.ps1
@@ -1,9 +1,10 @@
 #Requires -RunAsAdministrator
 
 # ($NULL -eq $IsWindows) checks for Windows Sandbox enviroment
-if($IsWindows -or ($NULL -eq $IsWindows)) {
+if ($IsWindows -or ($NULL -eq $IsWindows)) {
     Write-Host "Installing PowerShell Core"
-} else {
+}
+else {
     throw "Not Windows"
 }
 
@@ -11,7 +12,8 @@ if (Get-AppPackage -name "Microsoft.DesktopAppInstaller") {
     Write-Host "WinGet present" -ForegroundColor DarkGreen
     Write-Host "Installing PowerShell Core via WinGet" -ForegroundColor DarkYellow 
     winget install -e --id Microsoft.PowerShell --accept-source-agreements --accept-package-agreements --source winget
-} else {
+}
+else {
     Write-Host "WinGet missing..."  -ForegroundColor DarkYellow
     Write-Host "Checking for Chocolatey presence..."  -ForegroundColor DarkYellow
     Start-Sleep 10
@@ -21,12 +23,13 @@ if (Get-AppPackage -name "Microsoft.DesktopAppInstaller") {
         Write-Host "Chocolatey is present."  -ForegroundColor DarkGreen
         Write-Host "Installing PowerShell Core via Chocolatey" -ForegroundColor DarkYellow
         choco install powershell-core -y --packageparameters '"/CleanUpPath"'
-    } else {
+    }
+    else {
         Write-Host "Chocolatey is missing."  -ForegroundColor DarkMagenta
         throw "Neither Winget nor Chocolatey present"
     }
 }
 
 Write-Host "List current Powershell Core version" -ForegroundColor DarkGreen
-pwsh {$PSVersionTable}
+pwsh { $PSVersionTable }
 pwsh
